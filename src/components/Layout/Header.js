de7b32c64d1c5d1c33c94e28a9507b07c7b0d4cd
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-28 19:29:15 -0400

    import header cart button and render isntead of button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='table full of delicious food' />
