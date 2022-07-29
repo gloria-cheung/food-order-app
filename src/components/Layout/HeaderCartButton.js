83212c39b15a50addfd1c251616ef66d5d272868
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-28 21:05:47 -0400

    set state in app.js and pass down function as prop to headercart button so cart component shows when clicked

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,8 +2,10 @@ import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const {onClick} = props;
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}><CartIcon /></span>
       <span>Your Cart</span>
       <span className={classes.badge}>3</span>
