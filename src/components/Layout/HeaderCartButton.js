2489c9c446a32f4d85f2a6386a20c38da8aab933
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-29 18:34:13 -0400

    import carcontext and usecontext from react to then use to display numcartitems

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,19 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   const {onClick} = props;
+  const cartCtx = useContext(CartContext);
+
+  const numCartItems = cartCtx.items.reduce((currNum, item) => {currNum + item.amount}, 0);
 
   return (
     <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}><CartIcon /></span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numCartItems}</span>
     </button>
   )
 };
