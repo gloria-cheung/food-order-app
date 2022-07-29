9604cfb15415f90ee05b7c1154728b49bd670e5d
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-28 20:57:11 -0400

    create Modal component and attach to div with id overlays

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,28 @@
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import classes from './Modal.module.css';
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  )
+};
+
+const portalLocation = document.getElementById('overlays');
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalLocation)}
+      {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalLocation)}
+    </Fragment>
+  )
+};
+
+export default Modal;
