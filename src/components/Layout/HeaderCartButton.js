5512b9b795a21570429dad40a02e8540cb9e6d9e
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-08-01 12:44:45 -0400

    add animation to cart button icon when item amount changes

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,10 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnHighlighted, setBtnHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
   const {onClick} = props;
 
@@ -11,8 +12,25 @@ const HeaderCartButton = (props) => {
     return currNum + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnHighlighted(false);
+    }, 300);
+
+    return() => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items])
+
   return (
-    <button className={classes.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={classes.icon}><CartIcon /></span>
       <span>Your Cart</span>
       <span className={classes.badge}>{numCartItems}</span>
