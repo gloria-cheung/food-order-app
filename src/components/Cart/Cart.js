e774a486b9c4856f7b89561091dc138df94374f2
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-08-01 11:16:17 -0400

    render totalamount instead of hardcode and only show order button if cart isnt empty

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,11 +2,15 @@ import { useContext } from 'react';
 
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
-import CartProvider from '../../store/Cart-Provider';
+import CartContext from '../../store/cart-context';
 
 const Cart = (props) => {
-  const cartCtx = useContext(CartProvider);
+  const cartCtx = useContext(CartContext);
   const {onClose} = props;
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  
+  const hasItems = cartCtx.items.length > 0;
  
   const cartItems = cartCtx.items
     .map(item => <li key={item.id}>{item.name}</li>);
@@ -18,11 +22,11 @@ const Cart = (props) => {
       </ul>
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={onClose} >Close</button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   )
