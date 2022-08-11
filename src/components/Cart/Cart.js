17a97aaa5976babe340ea6f1e0b19cf47235d0a0
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-08-11 10:52:20 -0400

    import checkout component and refactor to hid 2 buttons after click order and render checkout form

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,51 +1,69 @@
-import { useContext } from 'react';
+import { useContext, useState } from "react";
 
-import Modal from '../UI/Modal';
-import classes from './Cart.module.css';
-import CartContext from '../../store/cart-context';
-import CartItem from './CartItem';
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
   const cartCtx = useContext(CartContext);
-  const {onClose} = props;
+  const { onClose } = props;
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-  
+
   const hasItems = cartCtx.items.length > 0;
- 
+
   const removeItemHandler = (id) => {
     cartCtx.removeItem(id);
   };
 
   const addItemHandler = (item) => {
-    cartCtx.addItem({...item, amount: 1});
+    cartCtx.addItem({ ...item, amount: 1 });
   };
 
-  const cartItems = cartCtx.items
-    .map(item => (
-      <CartItem 
-        key={item.id} 
-        {...item} 
-        onAdd={() => {addItemHandler(item)}} 
-        onRemove={() => {removeItemHandler(item.id)}} 
-      />
-    ));
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const cartItems = cartCtx.items.map((item) => (
+    <CartItem
+      key={item.id}
+      {...item}
+      onAdd={() => {
+        addItemHandler(item);
+      }}
+      onRemove={() => {
+        removeItemHandler(item.id);
+      }}
+    />
+  ));
+
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
 
   return (
-    <Modal onClose={onClose} >
-      <ul className={classes['cart-items']}>
-        {cartItems}
-      </ul>
+    <Modal onClose={onClose}>
+      <ul className={classes["cart-items"]}>{cartItems}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={onClose} >Close</button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+      {isCheckout && <Checkout onCancel={onClose} />}
+      {!isCheckout && modalActions}
     </Modal>
-  )
+  );
 };
 
 export default Cart;
