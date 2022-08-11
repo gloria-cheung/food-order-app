5eee20e978a053954b1cb4ea12b2bd2dd95396b8
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-08-11 11:38:25 -0400

    add error message when checking validity of inputs

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,8 +1,19 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./Checkout.module.css";
 
+const isEmpty = (value) => value.trim() === "";
+const isFiveChars = (value) => value.trim().length === 5;
+
 const Checkout = (props) => {
+  const [formInputValidity, setFormInputValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
+
   const { onCancel } = props;
+
   const nameInputRef = useRef();
   const streetInputRef = useRef();
   const postalCodeInputRef = useRef();
@@ -15,28 +26,72 @@ const Checkout = (props) => {
     const enteredStreet = streetInputRef.current.value;
     const enteredPostalCode = postalCodeInputRef.current.value;
     const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+
+    setFormInputValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      postalCode: enteredPostalCodeIsValid,
+      city: enteredCityIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredPostalCodeIsValid &&
+      enteredCityIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
   };
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          formInputValidity.name ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" ref={nameInputRef} />
+        {!formInputValidity.name && <p>Please entere a valid name</p>}
       </div>
 
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          formInputValidity.street ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor="street">Street</label>
         <input type="text" id="street" ref={streetInputRef} />
+        {!formInputValidity.street && <p>Please entere a valid street</p>}
       </div>
 
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          formInputValidity.postalCode ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor="postal">Postal Code</label>
         <input type="text" id="postal" ref={postalCodeInputRef} />
+        {!formInputValidity.postalCode && (
+          <p>Please entere a valid postal code</p>
+        )}
       </div>
 
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          formInputValidity.city ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor="city">City</label>
         <input type="text" id="city" ref={cityInputRef} />
+        {!formInputValidity.city && <p>Please entere a valid city</p>}
       </div>
 
       <div className={classes.actions}>
